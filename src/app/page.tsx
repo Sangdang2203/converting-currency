69ebdb7eabba0304040c56e97e34e11e8fc97752
Đặng Văn Sang <122693847+Sangdang2203@users.noreply.github.com>
2025-03-16 23:41:35 +0700

    update UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import { Metadata } from "next";
 import FancyFormComponent from "./(public)/FancyForm";
 
+export const metadata: Metadata = {
+  title: "Exchange Currency",
+  keywords: ["exchange currency", "currency conversion"],
+  robots: "index, follow",
+  description: "",
+};
 const Page = () => {
   return (
     <>
